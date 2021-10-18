d9f9bc7728a93e89e8439dc31ad3ab692a02275d
adimertaaa <adimerta@student.unud.ac.id>
2021-10-18 22:39:21 +0800

    gg

--- app/Http/Controllers/FrontOffice/CustomerBengkelController.php
+++ app/Http/Controllers/FrontOffice/CustomerBengkelController.php
@@ -57,11 +57,10 @@ class CustomerBengkelController extends Controller
     {
         $customer = CustomerBengkel::find($id_customer_bengkel);
 
-        $service = PenerimaanService::with('detail_sparepart','detail_perbaikan','kendaraan','bengkel')->join('tb_fo_customer_bengkel','tb_service_advisor.id_customer_bengkel','tb_fo_customer_bengkel.id_customer_bengkel')
+        $service = PenerimaanService::with(['detail_sparepart','detail_perbaikan','kendaraan','bengkel'])->join('tb_fo_customer_bengkel','tb_service_advisor.id_customer_bengkel','tb_fo_customer_bengkel.id_customer_bengkel')
         ->where('tb_service_advisor.id_customer_bengkel', $id_customer_bengkel)->where('tb_service_advisor.status','=','selesai_pembayaran')
         ->get();
 
-        return $service;
 
         $penjualan = PenjualanSparepart::with('Detailsparepart')->join('tb_fo_customer_bengkel','tb_fo_penjualan_sparepart.id_customer_bengkel','tb_fo_customer_bengkel.id_customer_bengkel')
         ->where('tb_fo_penjualan_sparepart.id_customer_bengkel', $id_customer_bengkel)->where('tb_fo_penjualan_sparepart.status_bayar','=', 'Lunas')
