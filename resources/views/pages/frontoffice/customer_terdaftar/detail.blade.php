3a101f68f1865f96c075300751ecfc8cc37bc584
adimertaaa <adimerta@student.unud.ac.id>
2021-10-18 22:46:38 +0800

    gg

--- resources/views/pages/frontoffice/customer_terdaftar/detail.blade.php
+++ resources/views/pages/frontoffice/customer_terdaftar/detail.blade.php
@@ -140,8 +140,8 @@
                                             @forelse ($service as $item)
                                             <tr role="row" class="odd">
                                                 <th scope="row" class="small" class="sorting_1">{{ $loop->iteration}}</th>
-                                                <td>{{ $item->detail_perbaikan->nama_jenis_perbaikan }}</td>
-                                                <td>{{ $item->detail_perbaikan->group_jenis_perbaikan }}</td>
+                                                <td>{{ $item->detail_perbaikan->nama_jenis_perbaikan ?? '' }}</td>
+                                                <td>{{ $item->detail_perbaikan->group_jenis_perbaikan ?? '' }}</td>
                                             </tr>
                                             @empty
                                           
