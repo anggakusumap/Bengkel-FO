f53f6cb0225ec6bd2bf06f5e251bd3237607c1bd
adimertaaa <adimerta@student.unud.ac.id>
2021-12-22 17:47:06 +0800

    gg

--- app/Http/Controllers/FrontOffice/PenjualanSparepartController.php
+++ app/Http/Controllers/FrontOffice/PenjualanSparepartController.php
@@ -22,7 +22,7 @@ class PenjualanSparepartController extends Controller
      */
     public function index()
     {
-        if(Auth::user()->pegawai->jabatan->nama_jabatan == 'Owner'){
+        if(Auth::user()->pegawai->jabatan->nama_jabatan == 'Owner' || Auth::user()->pegawai->jabatan->nama_jabatan == 'Front Office'){
             $penjualan = PenjualanSparepart::with(['Customer', 'Pegawai','Cabang'])->where('id_bengkel', Auth::user()->bengkel->id_bengkel)->where('id_cabang', '=', null)->orderBy('id_penjualan_sparepart', 'DESC')->get();
         }else{
             $penjualan = PenjualanSparepart::with(['Customer', 'Pegawai','Cabang'])->where('id_bengkel', Auth::user()->bengkel->id_bengkel)->where('id_cabang', Auth::user()->pegawai->cabang->id_cabang)->orderBy('id_penjualan_sparepart', 'DESC')->get();
